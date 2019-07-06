831349ab97343bb87f5d22a2bcb2741fe3c89bad
mmghunaim <51181545+mmghunaim@users.noreply.github.com>
2019-07-07 01:57:09 +0300

    Install JWT Package and do all of it's stuff

--- app/User.php
+++ app/User.php
@@ -5,8 +5,9 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
-class User extends Authenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\Question;
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -39,5 +40,27 @@ class User extends Authenticatable
 
     public function questions(){
         return $this->hasMany(Question::class);
+    }
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,17 @@ Route::post('/like/{reply}','LikeController@like');
 
 Route::delete('/like/{reply}','LikeController@unLike');
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
